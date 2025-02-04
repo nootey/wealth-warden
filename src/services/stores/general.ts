9812ba99039f4ee82dc870504cabfe215f426a1d
Nootey <poiskusni@gmail.com>
2025-02-04 20:23:12 +0100

    define general store

---
+++ src/services/stores/general.ts
@@ -0,0 +1,7 @@
+import {defineStore} from "pinia";
+import apiClient from './api/axios_interceptor.ts';
+export const useGeneralStore = defineStore('general', {
+    state: () => ({}),
+    actions: {
+    }
+});
