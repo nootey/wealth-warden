03b74ec36b6ecfa24abed4a322617c9abbaa19df
Nootey33 <poiskusni@gmail.com>
2025-03-06 22:17:52 +0100

    chore: minor updates to migration
    - ensure capitalization
    - ensure only one role is available to user_org link
    - rename super admin seeder

--- pkg/database/migrations/20250204134129_create_organization_users_table.sql
+++ pkg/database/migrations/20250204134129_create_organization_users_table.sql
@@ -1,17 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE organization_users (
+id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 user_id BIGINT UNSIGNED NOT NULL,
 organization_id BIGINT UNSIGNED NOT NULL,
 role_id BIGINT UNSIGNED NOT NULL,
 deleted_at TIMESTAMP DEFAULT null,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (user_id, organization_id),
 CONSTRAINT fk_user_org_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
 CONSTRAINT fk_user_org_organization FOREIGN KEY (organization_id) REFERENCES organizations(id) ON DELETE CASCADE,
 CONSTRAINT fk_user_org_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
-UNIQUE (user_id, organization_id, role_id)
+UNIQUE (user_id, organization_id)
 );
 -- +goose StatementEnd
 
