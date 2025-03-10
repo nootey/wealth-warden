c6ff8cc9b7b30be1ec7d3c126c5050ad50dd1c7d
Nootey33 <poiskusni@gmail.com>
2025-03-10 20:32:49 +0100

    feat: define initial dashboard composition
    - these are all just placeholders

--- src/components/Dashboard/DashboardIndex.vue
+++ src/components/Dashboard/DashboardIndex.vue
@@ -1,15 +1,57 @@
 <script setup lang="ts">
-import {useAuthStore} from "../../services/stores/authStore.ts";
+import MonthlyBudget from "../Shared/MonthlyBudget.vue";
 
-const authStore = useAuthStore();
+
 </script>
 
 <template>
   <main>
-    <h1>Home</h1>
-    <p> This is the home page </p>
-    <span> {{ "Has user initialized budget? "}}</span>
-    <span> {{ authStore?.user?.secrets?.budget_initialized}}</span>
+    <h1>Wealth Warden</h1>
+    <div class="flex flex-column w-100 gap-3">
+
+      <div class="flex flex-row w-100 gap-2">
+        <div class="flex flex-column gap-1">
+          <p> Selected year </p>
+          <div> {{ "2025" }}</div>
+        </div>
+      </div>
+
+
+      <div class="flex flex-row w-100 gap-2">
+        <div class="flex flex-column gap-1">
+          <p> Active budget </p>
+          <MonthlyBudget></MonthlyBudget>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="flex flex-row w-100 gap-2">
+        <div class="flex flex-column gap-1">
+          <p> Stats </p>
+          <span>These are the stats for the selected year, like: total inflows, effective inflows, take home, overflow ... Default is current year.</span>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="flex flex-row w-100 gap-2">
+        <div class="flex flex-column gap-1">
+          <p> Stats by category </p>
+          <span>These are the stats for each category for the selected year ... Default is current year.</span>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="flex flex-row w-100 gap-2">
+        <div class="flex flex-column gap-1">
+          <p> Saving rates </p>
+          <span>These are monthly rates for savings and investments for the selected year ... Default is current year.</span>
+        </div>
+      </div>
+
+    </div>
   </main>
 </template>
 
