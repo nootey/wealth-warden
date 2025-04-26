61291b7118f4bf938ae9ae36c46bacb62d047fcc
Nootey <poiskusni@gmail.com>
2025-04-26 19:51:03 +0200

    chore: correct loging form json casing

--- internal/models/auth.go
+++ internal/models/auth.go
@@ -3,5 +3,5 @@ package models
 type LoginForm struct {
 	Email      string `json:"email" binding:"required,email"`
 	Password   string `json:"password" binding:"required"`
-	RememberMe bool   `json:"rememberMe"`
+	RememberMe bool   `json:"remember_me"`
 }
