1ca0ffcb6b983088a80bc5f6e76543fdbe34fc20
Nootey <poiskusni@gmail.com>
2025-08-16 15:22:22 +0200

    chore: decouple vueHelper into toastHelper

--- src/utils/vueHelper.ts
+++ src/utils/vueHelper.ts
@@ -47,47 +47,6 @@ const vueHelper = {
 
         return num.toFixed(1) + " %";
     },
-    formatSuccessToast(title: string, msg: string) {
-        let message = {
-            'data': {
-                'messages': {
-                    'success': [] as string[]
-                },
-                'title': {}
-            }
-        }
-        message['data']['title'] = title;
-        message['data']['messages']['success'].push(msg);
-        return message;
-    },
-    formatInfoToast(title: string, msg: string) {
-        let message = {
-            'data': {
-                'messages': {
-                    'info': [] as string[]
-                },
-                'title': {}
-            }
-        }
-        message['data']['title'] = title;
-        message['data']['messages']['info'].push(msg);
-        return message;
-    },
-    formatErrorToast(title: string, msg: string){
-        let message = {
-            'response': {
-                'data': {
-                    'messages': {
-                        'error': [] as string[]
-                    },
-                    'title': {}
-                }
-            }
-        }
-        message['response']['data']['title'] = title;
-        message['response']['data']['messages']['error'].push(msg);
-        return message;
-    },
     formatChanges(payload: any) {
         if (payload === "[]") return null;
         if (payload) {
