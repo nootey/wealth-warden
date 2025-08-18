66dba2ede3c6df5429eab6f36fb368016f31bad3
Nootey <poiskusni@gmail.com>
2025-08-18 22:20:09 +0200

    feat: add helper methods for comparing changes

--- pkg/utils/logging_helper.go
+++ pkg/utils/logging_helper.go
@@ -1,8 +1,17 @@
 package utils
 
+import (
+	"github.com/shopspring/decimal"
+	"time"
+)
+
 type Changes struct {
 	New map[string]string
 	Old map[string]string
+}
+
+func (c *Changes) IsEmpty() bool {
+	return len(c.New) == 0 && len(c.Old) == 0
 }
 
 func InitChanges() *Changes {
@@ -22,6 +31,20 @@ func CompareChanges(old, new string, obj *Changes, index string) {
 			obj.Old[index] = old
 			obj.New[index] = new
 		}
+	}
+}
+
+func CompareDecimalChange(oldV, newV decimal.Decimal, changes *Changes, key string, places int32) {
+	if !oldV.Equal(newV) {
+		CompareChanges(oldV.StringFixed(places), newV.StringFixed(places), changes, key)
+	}
+}
+
+func CompareDateChange(oldT, newT time.Time, changes *Changes, key string) {
+	oldDate := oldT.UTC().Format("2006-01-02")
+	newDate := newT.UTC().Format("2006-01-02")
+	if oldDate != newDate {
+		CompareChanges(oldDate, newDate, changes, key)
 	}
 }
 
