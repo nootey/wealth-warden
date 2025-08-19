ea2ba7d1683e0e85415e8b108a1a903ef6c096d8
Nootey <poiskusni@gmail.com>
2025-08-19 14:39:56 +0200

    feat: implement primevue auto component import
    - also clean up main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// Styles
 import '../node_modules/primeflex/primeflex.css';
 import './style/global.scss'
 import './style/overrride.scss'
@@ -6,59 +7,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./services/router";
 import { createPinia } from 'pinia';
+
+// PrimeVue core + theme
 import PrimeVue from 'primevue/config';
 import Material from '@primevue/themes/material';
-import Tooltip from "primevue/tooltip";
-import {Chip, Toast} from "primevue";
-import {ToastService} from "primevue";
+
+// PrimeVue services & directives
 import ConfirmationService from 'primevue/confirmationservice';
-import {Button} from "primevue";
-import {Checkbox} from "primevue";
-import {InputText} from "primevue";
-import {FloatLabel} from "primevue";
-import {DataTable} from "primevue";
-import {Column} from "primevue";
-import {AutoComplete} from "primevue";
-import {InputGroup} from "primevue";
-import {InputGroupAddon} from "primevue";
-import {DatePicker} from "primevue";
-import {InputNumber} from "primevue";
-import {Paginator} from "primevue";
-import {Dialog} from "primevue";
-import {MultiSelect} from "primevue";
-import {ProgressSpinner} from "primevue";
-import {ConfirmPopup} from "primevue";
-import {Popover} from "primevue";
-import {SelectButton} from "primevue";
-import {IftaLabel} from "primevue";
+import Tooltip from "primevue/tooltip";
+import Ripple from 'primevue/ripple';
+import ToastService from 'primevue/toastservice';
 
+// App
 const app = createApp(App);
-const pinia = createPinia();
 
-app.directive('tooltip', Tooltip);
-app.component("Toast", Toast);
-app.component("Button", Button);
-app.component("Checkbox", Checkbox);
-app.component("InputText", InputText);
-app.component("FloatLabel", FloatLabel);
-app.component("DataTable", DataTable);
-app.component("Column", Column);
-app.component("AutoComplete", AutoComplete);
-app.component("InputGroup", InputGroup);
-app.component("InputGroupAddon", InputGroupAddon);
-app.component("DatePicker", DatePicker);
-app.component("InputNumber", InputNumber);
-app.component("Paginator", Paginator);
-app.component("Dialog", Dialog);
-app.component("MultiSelect", MultiSelect);
-app.component("ProgressSpinner", ProgressSpinner);
-app.component("ConfirmPopup", ConfirmPopup);
-app.component("Popover", Popover);
-app.component("SelectButton", SelectButton);
-app.component("Chip", Chip);
-app.component("IftaLabel", IftaLabel);
-
-app.use(pinia);
+// Plugins
+app.use(createPinia());
 app.use(router);
 app.use(PrimeVue, {
     theme: {
@@ -68,12 +32,15 @@ app.use(PrimeVue, {
             darkModeSelector: '.my-app-dark',
             cssLayer: false
         }
-    }
+    },
+    ripple: true
 });
+
 app.use(ToastService);
 app.use(ConfirmationService);
 
+// Directives
+app.directive('tooltip', Tooltip);
+app.directive('ripple', Ripple);
 
-
-app.component('App', App);
 app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dotenv from 'dotenv';
 dotenv.config();
+import Components from 'unplugin-vue-components/vite';
+import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 
 export default defineConfig({
 
-  plugins: [vue()],
+  plugins: [
+      vue(),
+    Components({
+      resolvers: [
+        PrimeVueResolver()
+      ]
+    })
+  ],
   server: {
     port: Number(process.env.VITE_APP_PORT) || 5173, // Use process.env directly
   },
