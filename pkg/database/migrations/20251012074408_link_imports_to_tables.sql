76c2d6eed6dc162d106a8bd3f71cd1c30af9dd33
Nootey <poiskusni@gmail.com>
2025-10-30 16:36:27 +0100

    chore: add import id to accounts and categories

--- pkg/database/migrations/20251012074408_link_imports_to_tables.sql
+++ pkg/database/migrations/20251012074408_link_imports_to_tables.sql
@@ -5,6 +5,12 @@ ALTER TABLE transactions
 
 ALTER TABLE transfers
     ADD COLUMN import_id BIGINT NULL;
+
+ALTER TABLE accounts
+    ADD COLUMN import_id BIGINT NULL;
+
+ALTER TABLE categories
+    ADD COLUMN import_id BIGINT NULL;
 -- +goose StatementEnd
 
 -- +goose Down
@@ -14,4 +20,10 @@ ALTER TABLE transactions
 
 ALTER TABLE transfers
 DROP COLUMN IF EXISTS import_id;
+
+ALTER TABLE accounts
+    DROP COLUMN IF EXISTS import_id;
+
+ALTER TABLE categories
+    DROP COLUMN IF EXISTS import_id;
 -- +goose StatementEnd
