44c19697e50b01860b76e455c7701e05a396c783
Nootey <poiskusni@gmail.com>
2025-12-04 09:28:00 +0100

    chore: setup interfaces to be mocked - account handler

--- pkg/validators/main.go
+++ pkg/validators/main.go
@@ -4,6 +4,10 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+type Validator interface {
+	ValidateStruct(data interface{}) error
+}
+
 type GoValidator struct {
 	Validator *validator.Validate
 }
@@ -11,6 +15,8 @@ type GoValidator struct {
 func NewValidator() *GoValidator {
 	return &GoValidator{Validator: validator.New()}
 }
+
+var _ Validator = (*GoValidator)(nil)
 
 func (cv *GoValidator) ValidateStruct(data interface{}) error {
 	return cv.Validator.Struct(data)
