19242a04a971fca78919b64bec91025a81ca6903
Nootey <poiskusni@gmail.com>
2025-12-12 11:24:29 +0100

    chore: add another docker compose for local deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
 
-    const DEV_PORT = Number(env.VITE_DEV_PORT) ?? 3000
+    const DEV_PORT = Number(env.VITE_DEV_PORT) ?? 5000
     const API_PROXY_TARGET = env.VITE_API_PROXY_TARGET ?? "http://localhost:2000"
 
     return {
