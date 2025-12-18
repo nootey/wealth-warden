8eeb2c67b47f13793b001be7def755cb3e657de4
Nootey <poiskusni@gmail.com>
2025-12-18 10:28:26 +0100

    chore: update range panel filter
    - toggle range state

--- client/src/_vue/components/filters/panels/RangePanel.vue
+++ client/src/_vue/components/filters/panels/RangePanel.vue
@@ -15,18 +15,20 @@ const operators = [
   { name: 'Less than',     value: '<=' as OpVal },
 ];
 
-function opObjFromValue(v: OpVal|undefined) {
-  return operators.find(o => o.value === (v ?? '=')) ?? operators[0];
-}
+const useRange = ref(false);
+
+const singleDisabled = computed(() => useRange.value);
+const rangeDisabled  = computed(() => !useRange.value);
 
 const selectedOperator = ref(opObjFromValue(model.value.singleOp));
 const filteredOperators = ref<typeof operators>([...operators]);
 
-const singleDisabled = computed(() => model.value.min !== null || model.value.max !== null);
-const rangeDisabled  = computed(() => singleVal.value !== null);
-
 watch(() => model.value.single, v => { if (v !== singleVal.value) singleVal.value = v ?? null; });
 watch(() => model.value.singleOp, v => { selectedOperator.value = opObjFromValue(v); });
+
+function opObjFromValue(v: OpVal|undefined) {
+    return operators.find(o => o.value === (v ?? '=')) ?? operators[0];
+}
 
 function onSingleInput(v: number|null) {
   singleVal.value = v;
@@ -69,38 +71,42 @@ const searchOperator = (event: any) => {
 </script>
 
 <template>
-  <div class="flex flex-column gap-2 w-full">
-
-    <div class="flex flex-row w-full">
-      <AutoComplete size="small" class="w-full" :modelValue="selectedOperator"
-          :disabled="singleDisabled" :suggestions="filteredOperators"   optionLabel="name"
-          placeholder="Select operator" dropdown @complete="searchOperator" @item-select="onOpSelect"/>
-    </div>
-
-    <div class="flex flex-row w-full">
-      <IftaLabel class="w-full">
-        <InputNumber size="small" class="w-full" inputId="single" :disabled="singleDisabled" :modelValue="singleVal" @update:modelValue="onSingleInput"
-            mode="currency" currency="EUR" locale="de-DE" placeholder="0,00 €"/>
-        <label for="single">Single value</label>
-      </IftaLabel>
-    </div>
-
-    <hr>
-
     <div class="flex flex-column gap-2 w-full">
-      <IftaLabel>
-        <InputNumber size="small" class="w-full" inputId="range_min" :disabled="rangeDisabled" :modelValue="model.min" @update:modelValue="onRangeMin"
-            mode="currency" currency="EUR" locale="de-DE" placeholder="0,00 €"/>
-        <label for="range_min">Min</label>
-      </IftaLabel>
 
-      <IftaLabel>
-        <InputNumber size="small" class="w-full" inputId="range_max" :disabled="rangeDisabled" :modelValue="model.max" @update:modelValue="onRangeMax"
-            mode="currency" currency="EUR" locale="de-DE" placeholder="0,00 €"/>
-        <label for="range_max">Max</label>
-      </IftaLabel>
+        <div v-if="!useRange" class="flex flex-row w-full">
+            <AutoComplete size="small" class="w-full" :modelValue="selectedOperator"
+                          :disabled="singleDisabled" :suggestions="filteredOperators"   optionLabel="name"
+                          placeholder="Select operator" dropdown @complete="searchOperator" @item-select="onOpSelect"/>
+        </div>
+
+        <div v-if="!useRange" class="flex flex-row w-full">
+            <IftaLabel class="w-full">
+                <InputNumber size="small" class="w-full" inputId="single" :disabled="singleDisabled" :modelValue="singleVal" @update:modelValue="onSingleInput"
+                             mode="currency" currency="EUR" locale="de-DE" placeholder="0,00 €"/>
+                <label for="single">Single value</label>
+            </IftaLabel>
+        </div>
+
+        <div v-if="useRange" class="flex flex-column gap-2 w-full">
+            <IftaLabel>
+                <InputNumber size="small" class="w-full" inputId="range_min" :disabled="rangeDisabled" :modelValue="model.min" @update:modelValue="onRangeMin"
+                             mode="currency" currency="EUR" locale="de-DE" placeholder="0,00 €"/>
+                <label for="range_min">Min</label>
+            </IftaLabel>
+
+            <IftaLabel>
+                <InputNumber size="small" class="w-full" inputId="range_max" :disabled="rangeDisabled" :modelValue="model.max" @update:modelValue="onRangeMax"
+                             mode="currency" currency="EUR" locale="de-DE" placeholder="0,00 €"/>
+                <label for="range_max">Max</label>
+            </IftaLabel>
+        </div>
+
+        <div class="flex align-items-center gap-2">
+            <Checkbox v-model="useRange" inputId="useRange" binary />
+            <label for="useRange">Use range</label>
+        </div>
+
     </div>
-  </div>
 </template>
 
 <style scoped></style>
