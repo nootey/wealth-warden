66f39f9a4922208645b8bb2eea21e8da888fa7ea
Nootey <poiskusni@gmail.com>
2025-12-18 13:07:51 +0100

    fix: confirm email route should be public

--- internal/http/routes/auth_routes.go
+++ internal/http/routes/auth_routes.go
@@ -1,8 +1,9 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
 	"wealth-warden/internal/http/handlers"
+
+	"github.com/gin-gonic/gin"
 )
 
 func PublicAuthRoutes(apiGroup *gin.RouterGroup, handler *handlers.AuthHandler) {
@@ -14,10 +15,10 @@ func PublicAuthRoutes(apiGroup *gin.RouterGroup, handler *handlers.AuthHandler) 
 	apiGroup.POST("/request-password-reset", handler.RequestPasswordReset)
 	apiGroup.GET("/validate-password-reset", handler.ValidatePasswordReset)
 	apiGroup.POST("/reset-password", handler.ResetPassword)
+	apiGroup.GET("/confirm-email", handler.ConfirmEmail)
 }
 
 func AuthRoutes(apiGroup *gin.RouterGroup, handler *handlers.AuthHandler) {
 	apiGroup.GET("/current", handler.GetAuthUser)
-	apiGroup.GET("/confirm-email", handler.ConfirmEmail)
 	apiGroup.POST("/resend-confirmation-email", handler.ResendConfirmationEmail)
 }
