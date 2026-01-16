8d47672ba84581b66563874eecb53b1564b7e98a
xBlaz3kx <blaz.dular@gmail.com>
2026-01-16 23:55:42 +0100

    fix: add option to specify config path via command instead of defaulting

--- cmd/root.go
+++ cmd/root.go
@@ -13,29 +13,30 @@ import (
 var (
 	cfg    *config.Config
 	logger *zap.Logger
+	configPath string
 )
+
 var rootCmd = &cobra.Command{
 	Use:     "wealth-warden",
 	Short:   "WealthWarden server",
 	Version: fmt.Sprintf("%s (commit: %s, built: %s)", version.Version, version.CommitSHA, version.BuildTime),
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		// Load config for every child command before they are executed
+		var err error
+		cfg, err = config.LoadConfig(&configPath)
+		return fmt.Errorf("Failed to load configuration: %s", err.Error())
+	},
 }
 
 func init() {
 	rootCmd.AddCommand(httpServerCmd)
 	rootCmd.AddCommand(migrateCmd)
 	rootCmd.AddCommand(seedCmd)
+
+	rootCmd.PersistentFlags().StringVarP(&configPath, "config", "c", "", "Path to configuration file")
 }
 
 func Execute() {
-
-	var err error
-
-	// Load config
-	cfg, err = config.LoadConfig(nil)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to load configuration: %s", err.Error()))
-	}
-
 	// Define logger and pass it into cobra commands
 	logger = logging.InitLogger(cfg.Release)
 	defer func() {
