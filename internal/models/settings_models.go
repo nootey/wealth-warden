1c5dfbf10eb8ddf8d61be22dd9533c43be5745e7
Nootey <poiskusni@gmail.com>
2026-05-09 12:45:02 +0200

    feat: add placeholder for selecting default sheet selector in settings
    - this is currently unused

--- internal/models/settings_models.go
+++ internal/models/settings_models.go
@@ -18,14 +18,15 @@ func (SettingsGeneral) TableName() string {
 }
 
 type SettingsUser struct {
-	UserID          int64     `gorm:"column:user_id;primaryKey" json:"id"`
-	Theme           string    `gorm:"column:theme" json:"theme"`
-	Accent          *string   `gorm:"column:accent" json:"accent"`
-	Language        string    `gorm:"column:language" json:"language"`
-	Timezone        string    `gorm:"column:timezone" json:"timezone"`
-	DefaultCurrency string    `gorm:"column:default_currency" json:"default_currency"`
-	CreatedAt       time.Time `gorm:"column:created_at" json:"created_at"`
-	UpdatedAt       time.Time `gorm:"column:updated_at" json:"updated_at"`
+	UserID               int64     `gorm:"column:user_id;primaryKey" json:"id"`
+	Theme                string    `gorm:"column:theme" json:"theme"`
+	Accent               *string   `gorm:"column:accent" json:"accent"`
+	Language             string    `gorm:"column:language" json:"language"`
+	Timezone             string    `gorm:"column:timezone" json:"timezone"`
+	DefaultCurrency      string    `gorm:"column:default_currency" json:"default_currency"`
+	DefaultSheetSeparator string   `gorm:"column:default_sheet_separator" json:"default_sheet_separator"`
+	CreatedAt            time.Time `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt            time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
 
 func (SettingsUser) TableName() string {
@@ -45,11 +46,12 @@ type CurrencyInfo struct {
 }
 
 type PreferenceSettingsReq struct {
-	Theme           string  `json:"theme"`
-	Accent          *string `json:"accent"`
-	Language        string  `json:"language"`
-	Timezone        string  `json:"timezone"`
-	DefaultCurrency string  `json:"default_currency"`
+	Theme                 string  `json:"theme"`
+	Accent                *string `json:"accent"`
+	Language              string  `json:"language"`
+	Timezone              string  `json:"timezone"`
+	DefaultCurrency       string  `json:"default_currency"`
+	DefaultSheetSeparator string  `json:"default_sheet_separator"`
 }
 
 type CompleteSetupReq struct {
