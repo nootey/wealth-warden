386fdf6febe18afd8efec305ae19f13723929aa0
nootey <poiskusni@gmail.com>
2026-09-16 18:34:19 +0200

    chore: auto confirm users if mailer is not present in dev mode

--- internal/bootstrap/service_container.go
+++ internal/bootstrap/service_container.go
@@ -88,7 +88,7 @@ func NewServiceContainer(cfg *config.Config, db *gorm.DB, rdb *redis.Client, log
 
 	// Initialize services
 	loggingService := services.NewLoggingService(loggingRepo)
-	authService := services.NewAuthService(logger.Named("auth_srv"), userRepo, roleRepo, settingsRepo, jobDispatcher, mail, sessionStore)
+	authService := services.NewAuthService(logger.Named("auth_srv"), userRepo, roleRepo, settingsRepo, jobDispatcher, mail, sessionStore, cfg.Release)
 	roleService := services.NewRolePermissionService(logger.Named("role_srv"), roleRepo, jobDispatcher)
 	userService := services.NewUserService(userRepo, roleRepo, jobDispatcher, mail)
 	accountService := services.NewAccountService(logger.Named("account_srv"), accountRepo, balanceRepo, transactionRepo, settingsRepo, savingsRepo, investmentRepo, jobDispatcher, priceFetcher)
